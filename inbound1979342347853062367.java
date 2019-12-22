329ad665f00d92e89eff766a70410284ea0c75d8
lelleeann <59148815+lelleeann@users.noreply.github.com>
2019-12-22 22:46:05 +0200

    Add files via upload

---
+++ inbound1979342347853062367.java
@@ -0,0 +1,19 @@
+package braddddff;
+
+public class brraaadd {
+public static void main (String args[]){
+System.out.println("Merry Christmas");
+char l = '+';
+char m = '*';
+char n = 'T';
+System.out. format ("%8c%n", l);
+System.out. format("%6c %c %c %n", l, m, l);
+System.out. format ("%4c %c %c %c %c %n", l, m, m, m, l);
+System.out. format ("%3c %c %c %c %c %c %n", l, m, m, m, m, l);
+System.out. format ("%3c %c %c %c %c %c %n", l, m, m, m, m, l);
+System.out.format ("%4c %c %c %c %c %n", l, m, m, m, l);
+System.out. format ("%6c %c %c %n", l, l, l);
+System.out. format ("%7c %c", n, n);
+  }
+}
+
